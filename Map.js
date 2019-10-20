854bd8d69e6ffebdf2b9f37bf50c466d2aad39d1
Fahad <fahad.majeed@hotmail.com>
2019-10-21 00:45:22 +0100

    Update Map.js

--- Map.js
+++ Map.js
@@ -1,33 +1,40 @@
 import React, {Component} from "react";
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-
-const mapStyles = {
-    width: '100%',
-    height: '70%',
-};
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 class  MapContainer extends Component {
     constructor(props) {
         super(props)
-        this.state = {
-            stores: [{lat: 51.5225733, lng: -0.1630078},
-                {latitude: 51.5225733, longitude: -0.1630078}]
-        }
+        this.state = {showingInfoWindow: false,  selectedPlace: {}, showingInfoWindow: false}
     }
 
     componentDidMount() {
+        //console.log('props,' ,this.props);
     }
     handleClickMarker() {
         console.log('click happened');
         this.props.clickAction();
     }
+
+    onMarkerClick = (props, marker, e) => {
+       this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+       })
+    }
+        
+
     displayMarkers = () => {
-        return this.state.stores.map((store, index) => {
+        return this.props.stores.map((store, index) => {
             return <Marker key={index} id={index} position={{
-                lat: store.latitude,
-                lng: store.longitude
+                lat: store.lat,
+                lng: store.lng
             }}
-            onClick={() => this.props.clickAction()} />
+            name={store.name}
+            icon={store.availability? "http://maps.google.com/mapfiles/ms/icons/green-dot.png":''}
+            onClick={this.onMarkerClick}
+            />
+
         })
     }
 
@@ -36,10 +43,25 @@ class  MapContainer extends Component {
             <Map
                 google={this.props.google}
                 zoom={10}
-                style={mapStyles}
+                className="Map"
                 initialCenter={{lat: 51.50, lng: -0.09}}
             >
-        {this.displayMarkers()}
+                {this.displayMarkers()}
+                <InfoWindow
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}>
+                    <div>
+                        <h4>{this.state.selectedPlace.name}</h4>
+                        <div className="resContainer" >
+                            <div className="imgHolder">
+                                <img className="infoWindowImg" src={"./locker-placeholder.png"} />
+                            </div>
+                            <div className="reserveDesc">
+                                <button className="btn btn-primary ">Reserve Now For £4.00</button>
+                            </div>
+                        </div>
+                    </div>
+                </InfoWindow>
             </Map>
         );
     }
